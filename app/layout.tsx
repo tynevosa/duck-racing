4fbcce16b5e9c0e1843364e4a077e3d181ae084d
tynevosa <michael.edy623@gmail.com>
2025-05-02 14:38:30 +0000

    fix: wrap root layout children with Suspense for better loading handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { SolanaProvider } from '@/proviers/solana';
+import { Suspense } from 'react';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SolanaProvider>{children}</SolanaProvider>
+        <SolanaProvider>
+          <Suspense>{children}</Suspense>
+        </SolanaProvider>
       </body>
     </html>
   );
